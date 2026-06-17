d3aca366e415932e152bd90d212d9cddb88ce654
oestemar <oestemar_a24@yahoo.co.jp>
2026-06-17 18:38:02 +0900

    add 1 line to config.py

--- config.py
+++ config.py
@@ -9,4 +9,5 @@ class Config:
     MYSQL_USER = os.environ.get('MYSQL_USER')
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
     MYSQL_DB = os.environ.get('MYSQL_DB')
+    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
     MYSQL_CURSORCLASS = 'DictCursor'
